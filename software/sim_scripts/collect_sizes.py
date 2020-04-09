2f7df54c6b141280c5bcced9e8a3a04490d72f68
Dan Fritchman <dan@fritch.mn>
2020-04-09 01:06:34 +0000

    Write some better fake YAML

--- software/sim_scripts/collect_sizes.py
+++ software/sim_scripts/collect_sizes.py
@@ -4,10 +4,10 @@ Run a few workloads, and write some YAML data of their matrix sizes
 import os 
 
 testcases = {
-  'template': '/chipyard/generators/gemmini/software/gemmini-rocc-tests/build_hw_tiler/bareMetalC/template-pk',
-  'cifar': '/chipyard/generators/gemmini/software/gemmini-rocc-tests/build_hw_tiler/ee290/cifar_quant-pk',
-  'mobilenet': '/chipyard/generators/gemmini/software/gemmini-rocc-tests/build_hw_tiler/imagenet/mobilenet-pk', 
-  'resnet50': '/chipyard/generators/gemmini/software/gemmini-rocc-tests/build_hw_tiler/imagenet/resnet50-pk',
+  'template': 'generators/gemmini/software/gemmini-rocc-tests/build_hw_tiler/bareMetalC/template-pk',
+  'cifar': 'generators/gemmini/software/gemmini-rocc-tests/build_hw_tiler/ee290/cifar_quant-pk',
+  'mobilenet': 'generators/gemmini/software/gemmini-rocc-tests/build_hw_tiler/imagenet/mobilenet-pk', 
+  #'resnet50': 'generators/gemmini/software/gemmini-rocc-tests/build_hw_tiler/imagenet/resnet50-pk',
 }
 
 resultfile = 'sizes.yml'
@@ -17,12 +17,13 @@ with open(resultfile, 'w') as results:
 for tname in testcases:
   targ = testcases[tname]
   with open(resultfile, 'a') as results:
-    results.write(f'  name: {tname} \n')
-    results.write(f'  sizes: \n')
+    results.write(f'  - \n') 
+    results.write(f'    name: {tname} \n')
+    results.write(f'    sizes: \n')
   
   # Yeah OK it's not much of a python script here is it 
   log_pattern = 'GEMMINI_SIZES:'
-  cmd = f'spike --extension=gemmini2 pk {targ} | spike-dasm | grep "{log_pattern}" | sed "s/{log_pattern}//" | tee -a {resultfile}'
+  cmd = f'spike --extension=gemmini2 pk {targ} | spike-dasm | grep "{log_pattern}" | sed "s/{log_pattern}/  /" | tee -a {resultfile}'
   print(cmd)
   os.system(cmd)
 
