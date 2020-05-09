86b6d2412cf31def31c6f9bb37eba2c157b2f58f
Sam Steffl <sam@ssteffl.com>
2020-05-08 17:12:57 -0700

    added firesim gemm-sweep configs

--- software/overlay/root/run-gemm-sweep.sh
+++ software/overlay/root/run-gemm-sweep.sh
@@ -2,14 +2,22 @@
 
 LOGFILE=test_output.txt
 
-/root/ee290/gemm-linux -verify \
-  16  16  16 \
-  32  32  32 \
-  64  64  64 \
-  128 128 128 \
-  ... \
-  10000 10000 10000 \
-  | tee -a $LOGFILE
+MNK_VALUES="
+16  16  16
+32  32  32
+64  64  64
+128 128 128
+...
+10000 10000 10000
+"
+
+echo "=============TEST RESULTS=============" > $LOGFILE
+
+# this version does A*B+D=C
+/root/ee290/gemm-linux -verify $MNK_VALUES | tee -a $LOGFILE
+
+# this version does A*B=C
+/root/ee290/gemm-linux -verify -no_d $MNK_VALUES | tee -a $LOGFILE
 
 cat $LOGFILE
 poweroff -f
